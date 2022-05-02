2863724a0332672534f1636c5a6e0ba20de69b54
RicardoVCDev <gowfourth@hotmail.com>
2022-05-02 19:39:26 +0800

    HighestRiskScore Test

--- src/com/jon/learning/tests/FastestSearch.java
+++ src/com/jon/learning/tests/FastestSearch.java
@@ -1,8 +1,13 @@
-package com.jon.learning.search;
+package com.jon.learning.tests;
 
-public class BinaryTry {
+/**
+ * Given an array of integers sorted in ascending order,
+ * Search for the index of a given value in the fastest way possible.
+ * If the value is not, return -1.
+ */
+public class FastestSearch {
     public static void main(String[] args) {
-        int[] intArr = {-3,-2,-1,0,1,2,3,4,5,6,7};
+        int[] intArr = {-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7};
 
         System.out.println("6 is in position 9: " + search(intArr, 6));
         System.out.println("-2 is in position 1: " + search(intArr, -2));
@@ -12,19 +17,6 @@ public class BinaryTry {
     }
 
     private static int search(int[] input, int sought) {
-        int start = 0;
-        int end = input.length;
-
-        while (start < end) {
-            int mid = (start + end) / 2;
-            if (input[mid] == sought) return mid;
-            if (input[mid] < sought) {
-                start = mid + 1;
-            } else {
-                end = mid;
-            }
-        }
-
         return -1;
     }
 }
